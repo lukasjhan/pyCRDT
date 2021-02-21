534ea2bc107d5966d54cefc0966f921c5c739e84
UNKNOWN <unknown.zero.un@gmail.com>
2021-02-21 23:35:05 +0900

    Change GCounter merge logic

--- GCounter.py
+++ GCounter.py
@@ -1,5 +1,3 @@
-from itertools import chain
-
 GCounter = dict
 
 def zero():
@@ -12,8 +10,4 @@ def inc(c: GCounter, key):
     c[key] = c.get(key, 0) + 1
 
 def merge(a: GCounter, b:GCounter):
-    ret = GCounter()
-    for k, v in chain(a.items(), b.items()):
-        ret[k] = max(ret.get(k, 0), v)
-    return ret
-    
+    return { key: max(a.get(key, 0), b.get(key, 0)) for key in set(a.keys()) | set(b.keys()) }
